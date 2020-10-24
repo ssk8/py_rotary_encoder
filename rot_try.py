dd663ea7a34bb23ddaf916e43c6a0186dffe8e50
ssk8 <ssk8@users.noreply.github.com>
2020-10-23 22:56:46 -0500

    Update rot_try.py

--- rot_try.py
+++ rot_try.py
@@ -1,38 +1,27 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-A, B = 23, 24
-lastEncoded = 0
-encoderValue = 0
+Rot_pin_1, Rot_pin_2 = 23, 24
+lastEncoded, encoderValue = 0, 0
+
 
 def update_encoder(channel=0):
-    global lastEncoded
     global encoderValue
-
-    MSB = GPIO.input(A) == GPIO.LOW
-    LSB = GPIO.input(B) == GPIO.LOW
-
-    encoded = (MSB << 1) |LSB
-    sum  = (lastEncoded << 2) | encoded
-
-    if(sum == 0b1101 or sum == 0b0100 or sum == 0b0010 or sum == 0b1011): encoderValue +=1
-    if(sum == 0b1110 or sum == 0b0111 or sum == 0b0001 or sum == 0b1000): encoderValue -=1
-
+    global lastEncoded
+    MSB = GPIO.input(Rot_pin_1) == GPIO.LOW
+    LSB = GPIO.input(Rot_pin_2) == GPIO.LOW
+    encoded = (MSB << 1) | LSB
+    summed  = (lastEncoded << 2) | encoded
+    if summed in (13, 4, 2, 11): encoderValue +=1
+    if summed in (14, 7, 1, 8): encoderValue -=1
     lastEncoded = encoded
 
 
-
 GPIO.setmode(GPIO.BCM)
-for pin in (A, B):
+for pin in (Rot_pin_1, Rot_pin_2):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=update_encoder)
 
-def buttonA():
-    return GPIO.input(A) == GPIO.LOW
-
-
-def buttonB():
-    return GPIO.input(B) == GPIO.LOW
 
 while True:
     print(encoderValue)
