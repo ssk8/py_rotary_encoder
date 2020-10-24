934e2089cdecb5514494b38f851faa4b46323d9f
ssk8 <ssk8@users.noreply.github.com>
2020-10-23 23:27:11 -0500

    Update rot_try.py

--- rot_try.py
+++ rot_try.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-Rot_pin_1, Rot_pin_2 = 23, 24
+rot_pin_1, rot_pin_2 = 23, 24
 lastEncoded, encoderValue = 0, 0
 
 
 def update_encoder(channel=0):
     global encoderValue
     global lastEncoded
-    MSB = GPIO.input(Rot_pin_1) == GPIO.LOW
-    LSB = GPIO.input(Rot_pin_2) == GPIO.LOW
+    MSB = GPIO.input(rot_pin_1) == GPIO.LOW
+    LSB = GPIO.input(rot_pin_2) == GPIO.LOW
     encoded = (MSB << 1) | LSB
     summed  = (lastEncoded << 2) | encoded
     if summed in (13, 4, 2, 11): encoderValue +=1
@@ -18,11 +18,15 @@ def update_encoder(channel=0):
 
 
 GPIO.setmode(GPIO.BCM)
-for pin in (Rot_pin_1, Rot_pin_2):
+for pin in (rot_pin_1, rot_pin_2):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=update_encoder)
 
+def main():
+    while True:
+        print(encoderValue)
+        sleep(1)
 
-while True:
-    print(encoderValue)
-    sleep(1)
+
+if __name__ == "__main__":
+    main()
